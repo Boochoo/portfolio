6114960716dbe90b3c6026a4d8379251f26f2114
boochoo <fromaddistohelsinki@gmail.com>
2018-01-05 08:41:42 +0200

    dummy texts added to sections

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,7 +6,21 @@ export default class Education extends React.Component {
     return (
       <div>
         <Header />
-        <h1>Education</h1>
+        <div className="card Header__content">
+          <div className="card__content">
+            <div className="card__title">Education</div>
+            <p className="card__text card__text-group">
+              <p className="card__text card__text-group">
+                Once up on a time in a far, far, far away land called Ethiopia
+                the journey of this mysterious... Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat
+              </p>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,21 @@ export default class Skills extends React.Component {
     return (
       <div>
         <Header />
-        <h1>Skills</h1>
+        <div className="card Header__content">
+          <div className="card__content">
+            <div className="card__title">Skills</div>
+            <p className="card__text card__text-group">
+              <p className="card__text card__text-group">
+                Once up on a time in a far, far, far away land called Ethiopia
+                the journey of this mysterious... Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat
+              </p>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
